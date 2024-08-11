31151e09025de7d56218891ea4d039c8a6eaa18b
emil-eberhard-stretch <emil.eberhard@stretch.se>
2024-08-11 05:00:15 +0200

    hide admin tab for non admins

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -2,21 +2,22 @@ import { Tabs } from 'expo-router';
 import { useColorScheme } from 'react-native';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
+import { useAuth } from '@/hooks/useAuth';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { user } = useAuth();
 
   return (
     <Tabs
-      initialRouteName="index"
       screenOptions={{
-        headerShown: false, // This will hide the header for all tab screens
+        headerShown: false,
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          headerShown: false, // This will hide the header for the Home tab
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           tabBarLabel: 'Hem',
         }}
@@ -26,6 +27,7 @@ export default function TabLayout() {
         options={{
           tabBarIcon: ({ color }) => <TabBarIcon name="people" color={color} />,
           tabBarLabel: 'Admin',
+          href: user?.admin ? '/tabs/admin' : null,
         }}
       />
       <Tabs.Screen
