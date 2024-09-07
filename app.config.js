d5358c5064d536e503c370b886fb31fd68a44dd7
Emil Eberhard <36962202+emileberhard@users.noreply.github.com>
2024-09-07 04:41:53 +0200

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -29,7 +29,7 @@ export default ({ config }) => {
     ...config,
     ...variantConfig,
     slug: "KarateKidcApp",
-    version: "1.5.6"
+    version: "1.5.6",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "karatekidcapp",
